21bfa303c9e03a61326474188006e41a645fb053
thovo <vttho1996@gmail.com>
2019-11-19 20:54:57 +0700

    Update the latest code and environment

--- 00_RISCV_design/01_rtl/02_pipeline/riscv_forwarding.v
+++ 00_RISCV_design/01_rtl/02_pipeline/riscv_forwarding.v
@@ -1,7 +1,7 @@
 //==================================================================================================
 //  Filename      : riscv_forwarding.v
 //  Created On    : 2019-09-11 20:54:29
-//  Last Modified : 2019-09-11 22:41:58
+//  Last Modified : 2019-11-19 00:45:00
 //  Revision      : 
 //  Author        : Tho Vo
 //  Email         : vttho1996@gmail.com
@@ -14,13 +14,16 @@
 `define R_TYPE      7'b01100_11
 `define I_TYPE      7'b00100_11
 `define LOAD_TYPE   7'b00000_11
-`define S_TYPE      7'b00100_11  
+`define S_TYPE      7'b01000_11  //Store
 
 module riscv_forwarding (/*autoport*/
 //output
      forward_A,
      forward_B,
+     forward_3_A,
+     forward_3_B,
 //input
+     inst_s2,
      inst_s3,
      inst_s4,
      inst_s5
@@ -34,6 +37,8 @@ module riscv_forwarding (/*autoport*/
   input  [31:0] inst_s4;
   input  [31:0] inst_s5;
   output [1:0] forward_A;
+  output       forward_3_A;
+  output       forward_3_B;
   output [1:0] forward_B;
 
   //=============================================
@@ -41,13 +46,14 @@ module riscv_forwarding (/*autoport*/
   //=============================================
   reg [1:0] forward_A_reg;
   reg [1:0] forward_B_reg;
+  reg       forward_3_A_reg;
+  reg       forward_3_B_reg;
 
   //=============================================
   // Architecture module 
   //=============================================
   always @ (*) begin
-    case (inst_s2[6:0])
-      `R_TYPE: begin
+    if ((inst_s2[6:0] == `R_TYPE) || (inst_s2[6:0] == `I_TYPE) || (inst_s2[6:0] == `LOAD_TYPE) || (inst_s2[6:0] == `S_TYPE)) begin
         if ( 
         ((inst_s3[6:0] == `R_TYPE) && (inst_s3[11:7] == inst_s2[19:15])) || 
         ((inst_s3[6:0] == `I_TYPE) && (inst_s3[11:7] == inst_s2[19:15]))
@@ -57,7 +63,7 @@ module riscv_forwarding (/*autoport*/
         else if (
         ((inst_s4[6:0] == `R_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) || 
         ((inst_s4[6:0] == `I_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) ||
-        ((inst_s4[6:0] == `LOAD_TYPE) && (inst_s4[11:7] == inst_s4[19:15]))
+        ((inst_s4[6:0] == `LOAD_TYPE) && (inst_s4[11:7] == inst_s2[19:15]))
         ) begin
           forward_A_reg = 2'b10;
         end
@@ -67,88 +73,24 @@ module riscv_forwarding (/*autoport*/
         ((inst_s5[6:0] == `LOAD_TYPE) && (inst_s5[11:7] == inst_s2[19:15]))
         ) begin
           forward_A_reg = 2'b11;
-        end
-        else begin
+    end
+    else begin
           forward_A_reg = 2'b00;
-        end
-      end
-      `I_TYPE: begin
-        if ( 
-        ((inst_s3[6:0] == `R_TYPE) && (inst_s3[11:7] == inst_s2[19:15])) || 
-        ((inst_s3[6:0] == `I_TYPE) && (inst_s3[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b01;
-        end
-        else if (
-        ((inst_s4[6:0] == `R_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) || 
-        ((inst_s4[6:0] == `I_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) ||
-        ((inst_s4[6:0] == `LOAD_TYPE) && (inst_s4[11:7] == inst_s4[19:15]))
-        ) begin
-          forward_A_reg = 2'b10;
-        end
-        else if (
-        ((inst_s5[6:0] == `R_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) || 
-        ((inst_s5[6:0] == `I_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) ||
-        ((inst_s5[6:0] == `LOAD_TYPE) && (inst_s5[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b11;
-        end
-        else begin
-          forward_A_reg = 2'b00;
-        end
-      end
-      `LOAD_TYPE: begin
-        if ( 
-        ((inst_s3[6:0] == `R_TYPE) && (inst_s3[11:7] == inst_s2[19:15])) || 
-        ((inst_s3[6:0] == `I_TYPE) && (inst_s3[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b01;
-        end
-        else if (
-        ((inst_s4[6:0] == `R_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) || 
-        ((inst_s4[6:0] == `I_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) ||
-        ((inst_s4[6:0] == `LOAD_TYPE) && (inst_s4[11:7] == inst_s4[19:15]))
-        ) begin
-          forward_A_reg = 2'b10;
-        end
-        else if (
-        ((inst_s5[6:0] == `R_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) || 
-        ((inst_s5[6:0] == `I_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) ||
-        ((inst_s5[6:0] == `LOAD_TYPE) && (inst_s5[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b11;
-        end
-        else begin
-          forward_A_reg = 2'b00;
-        end        
-      end
-      `S_TYPE: begin
-        if ( 
-        ((inst_s3[6:0] == `R_TYPE) && (inst_s3[11:7] == inst_s2[19:15])) || 
-        ((inst_s3[6:0] == `I_TYPE) && (inst_s3[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b01;
-        end
-        else if (
-        ((inst_s4[6:0] == `R_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) || 
-        ((inst_s4[6:0] == `I_TYPE) && (inst_s4[11:7] == inst_s2[19:15])) ||
-        ((inst_s4[6:0] == `LOAD_TYPE) && (inst_s4[11:7] == inst_s4[19:15]))
-        ) begin
-          forward_A_reg = 2'b10;
-        end
-        else if (
-        ((inst_s5[6:0] == `R_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) || 
-        ((inst_s5[6:0] == `I_TYPE) && (inst_s5[11:7] == inst_s2[19:15])) ||
-        ((inst_s5[6:0] == `LOAD_TYPE) && (inst_s5[11:7] == inst_s2[19:15]))
-        ) begin
-          forward_A_reg = 2'b11;
-        end
-        else begin
-          forward_A_reg = 2'b00;
-        end                
-      end
-      default: forward_A_reg = 2'b00; 
-    endcase
+    end
+  end
+
+  always @(*) begin
+    if ((inst_s3[6:0] == `R_TYPE) || (inst_s3[6:0] == `I_TYPE) || (inst_s3[6:0] == `LOAD_TYPE) || (inst_s3[6:0] == `S_TYPE)) begin
+       if (inst_s4[6:0] == `LOAD_TYPE) begin
+          forward_3_A_reg = 1'b1;
+       end
+       else begin
+          forward_3_A_reg = 1'b0;
+       end 
+    end
+    else begin
+          forward_3_A_reg = 1'b0;
+    end
   end
 
   //==================================================
@@ -185,10 +127,28 @@ module riscv_forwarding (/*autoport*/
     end
   end
 
+ ///////////////////////////////////////////
+ ///////////////////////////////////
+  always @ (*) begin
+    if (inst_s3[6:0] == `R_TYPE || inst_s3[6:0] == `S_TYPE) begin
+      if (inst_s4[6:0] == `LOAD_TYPE) begin
+        forward_3_B_reg = 1'b1;
+      end
+      else begin
+        forward_3_B_reg = 1'b0;
+      end
+    end
+    else begin
+        forward_3_B_reg = 1'b0;
+    end
+  end
+
   //=============================================
   // output port
   //=============================================
   assign forward_A = forward_A_reg;
   assign forward_B = forward_B_reg;
+  assign forward_3_A = forward_3_A_reg;
+  assign forward_3_B = forward_3_B_reg;
 
 endmodule 
